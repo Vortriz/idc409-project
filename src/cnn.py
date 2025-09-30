283f76f3ede6f2127cd8b35df7817cb1294279ad
Vortriz <vorarishi22+github@gmail.com>
2025-09-30 09:07:36 +0530

    create placeholder files

---
+++ src/cnn.py
@@ -0,0 +1,14 @@
+import marimo
+
+__generated_with = "0.17.2"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    # placeholder
+    return
+
+
+if __name__ == "__main__":
+    app.run()
